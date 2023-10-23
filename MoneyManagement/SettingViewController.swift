a4e1031a77cd3f252a7ea48f00853e311c951632
okami-ap <y.okami@alphapolis.co.jp>
2023-10-23 10:59:08 +0900

    目標設定追加

--- MoneyManagement/SettingViewController.swift
+++ MoneyManagement/SettingViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 import RealmSwift
 
 class SettingViewController: UIViewController {
+    // Realmインスタンスを取得する
+    let realm = try! Realm()
+    var task: Setting!
+    
     @IBOutlet weak var goalTextField: UITextField!
     @IBOutlet weak var incomeTextField: UITextField!
     @IBOutlet weak var registButton: UIButton!
     @IBOutlet weak var goalLavelField: UILabel!
-    
-    // Realmインスタンスを取得する
-    let realm = try! Realm()
-    var task: Setting!
     
     @objc func dismissKeyboard(){
         // キーボードを閉じる
@@ -30,16 +30,42 @@ class SettingViewController: UIViewController {
         self.goalLavelField.text = "\(month)月の目標設定"
     }
     
-
+    
     @IBAction func handleRegistButton(_ sender: Any) {
-        try! realm.write {
-            self.task.goal = self.goalTextField.text!
-            self.task.income = self.incomeTextField.text!
-            self.task.date = Date()
-            self.realm.add(self.task, update: .modified)
+        var regist = true
+        if Int(self.goalTextField.text!) == nil {
+            regist = false
         }
         
-        self.dismiss(animated: true, completion: nil)
+        if Int(self.incomeTextField.text!) == nil {
+            regist = false
+        }
+        
+        if regist {
+            task = Setting()
+            try! realm.write {
+                self.task.goal = self.goalTextField.text!
+                self.task.income = self.incomeTextField.text!
+                self.task.date = Date()
+                self.realm.add(self.task, update: .modified)
+            }
+            
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            // アラートを作成
+            let alertController = UIAlertController(
+                title: "入力値エラー",
+                message: "目標は数値のみ設定してください。",
+                preferredStyle: .alert
+            )
+            
+            // アクションボタンを追加
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            
+            // アラートを表示
+            present(alertController, animated: true, completion: nil)
+        }
     }
-
+    
 }
